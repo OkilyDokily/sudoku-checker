ce40480877d5dae78005a811fcc29b82e1b9c197
me <me@me.me.com>
2020-06-18 14:52:05 -0700

    second spec passes test

--- __tests__/sudoku-checker.test.js
+++ __tests__/sudoku-checker.test.js
@@ -1,8 +1,18 @@
-import {convertStringToArray} from '../src/sudoku-checker.js'
+import {convertStringToArray,convertArrayTo2DArray} from '../src/sudoku-checker.js'
 
 describe("convertStringToArray",()=>{
   test("Convert string of 81 digits to array of 81 digits",()=>{
     const array = convertStringToArray("123");
     expect(array).toEqual([1,2,3]);
+  });
+});
+
+
+describe("convertArrayTo2DArray",()=>{
+  test("Convert array of 81 digits to 2d array of 9 by 9",()=>{
+    const array = new Array(81).fill(1);
+    const result = convertArrayTo2DArray(array);
+    const testArray = [new Array(9).fill(1),new Array(9).fill(1),new Array(9).fill(1),new Array(9).fill(1),new Array(9).fill(1),new Array(9).fill(1),new Array(9).fill(1),new Array(9).fill(1),new Array(9).fill(1)];
+    expect(result).toEqual(testArray);
   });
 });
